b3c0073d9e953321d64ecede21b2f7f95459083c
Victor Buridard <victor.buridard@preligens.com>
2022-12-21 11:54:49 +0100

    fix: mock token

--- christmas-tree-api/src/app.controller.ts
+++ christmas-tree-api/src/app.controller.ts
@@ -38,8 +38,7 @@ export class AppController {
     // This is a mocked behaviour. This should never be used in production
     if (username === 'Santa' && password === 'Xmas') {
       return {
-        token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+        token: MOCK_TOKEN,
       };
     }
     throw new HttpException('Authentication failure', 401);
